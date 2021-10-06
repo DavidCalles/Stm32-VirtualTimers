40ca4646c3fc66efbd2aca82823cb729b5a2cc04
David Calles <yodavidcalles@gmail.com>
2021-10-05 23:15:37 -0400

    Add virtual timer instance initiator (Before test)

--- src/TimerCmd.c
+++ src/TimerCmd.c
@@ -10,6 +10,7 @@
 ---------------------------- LIBRARY DEFINITIONS --------------------------
 ***************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <ctype.h>
 
@@ -35,14 +36,17 @@
 typedef struct{
   uint32_t timeout;     // virtual counter timeout
   uint32_t current;     // virtual counter current value
+  uint32_t gpioPin;     // Pin to be toggled
   uint8_t flag;         // ready flag
-  uint8_t repetitive;   // count 1 single time or undefinetely
+  uint8_t enable;       // Current status of virtual timer
+  uint32_t repetitive;  // count 1 single time or undefinetely
+  uint8_t nodeID;
 }V_TIMER;
 
 // Linked list node structure
-typedef struct{
+typedef struct node{
   V_TIMER *vTimerPtr;
-  struct V_NODE *nextNode;
+  struct node *nextNode;
 }V_NODE;
 
 /**************************************************************************
@@ -50,6 +54,7 @@ typedef struct{
 ***************************************************************************/
 TIM_HandleTypeDef htim3;  // Timer Handler 
 V_NODE *head = NULL;      // head of linked list
+uint8_t countNodes = 0;
 
 /**************************************************************************
 ------------------------ OWN FUNCTION DEFINITIONS -------------------------
@@ -106,19 +111,140 @@ ParserReturnVal_t TimerInit()
   }
   return CmdReturnOk;
 }
+// MACRO: Add new command to help menu
+ADD_CMD("timerinit", TimerInit,"\t\tInitializes hardware timer.")
 
-// Function called when Timer3 sets an interrupt
+/*--------------------------------------------------------------------------
+*	Name:			    TimerInit
+*	Description:	
+*	Parameters:		void
+*
+*	Returns:		  ret: CmdReturnOk = 0 if Okay.
+---------------------------------------------------------------------------*/
+ParserReturnVal_t TimerInstance(int action)
+{ 
+  // Help messages
+  if(action==CMD_SHORT_HELP) return CmdReturnOk;
+  if(action==CMD_LONG_HELP) {
+    printf("timer <timeout> <out_pin> <recurrent>\n\n"
+	   "This command Initialize a virtual timer instance.\n"
+     "timeout: number of counts of the already initialized hardware timer"
+     "timebase.\n"
+     "out_pin: a digital pin can be toggled with virtual timer overflow.\n"
+     "recurrent: if recurrent 0, virtual timer will be disabled after first" 
+     "overflow event, if recurrent 1, timer will continue indefinitely.\n" 
+	  );
+    return CmdReturnOk;
+  }
+  // Get arguments from command line
+  uint32_t arguments[3] = {0};
+  for(int i=0; i<3; i++){
+    if(fetch_uint32_arg(&arguments[i])){
+      printf("Insuficient number of arguments. \n "
+      "Type <help timer> to get more information.\n");
+    }
+  }
+  
+  /* Add new node */
+  // Allocate memory for the node
+	V_NODE *newTimerPtr = NULL;
+	newTimerPtr = (V_NODE*)malloc(sizeof(V_NODE));
+	if (newTimerPtr == NULL) {
+		printf("Memory Allocation Error!\n");
+	}
+
+  /* Fill in new node */
+  newTimerPtr->vTimerPtr->timeout = arguments[0];
+  newTimerPtr->vTimerPtr->gpioPin = arguments[1]; 
+  newTimerPtr->vTimerPtr->repetitive = arguments[2];
+  newTimerPtr->vTimerPtr->flag = 0;
+  newTimerPtr->vTimerPtr->current = 0;
+  newTimerPtr->vTimerPtr->enable = 1;
+  newTimerPtr->vTimerPtr->nodeID = countNodes;
+
+  /* Position new node */
+  // Empty linked list
+  if(head == NULL){
+    newTimerPtr->nextNode = NULL;
+    head = newTimerPtr;
+  }
+  else{
+    // Traverse list and put at the end
+    V_NODE *currentTimerPtr = head;
+    while(currentTimerPtr->nextNode != NULL){
+      currentTimerPtr = currentTimerPtr->nextNode;
+    }
+    currentTimerPtr->nextNode = newTimerPtr;
+    newTimerPtr->nextNode = NULL;
+  }
+  printf("Virtual Timer ID: %d\n", countNodes);
+  countNodes++;
+
+  return CmdReturnOk;
+}
+// MACRO: Add new command to help menu
+ADD_CMD("timer", TimerInstance,"\t\tInitialize a virtual timer instance.")
+
+/*--------------------------------------------------------------------------
+*	Name:			    TimerInit
+*	Description:	
+*	Parameters:		void
+*
+*	Returns:		  ret: CmdReturnOk = 0 if Okay.
+---------------------------------------------------------------------------*/
+void VirtualTimers()
+{ 
+  // Traverse all virtual timer instances
+  V_NODE *currentPtr = head;
+
+  while(currentPtr != NULL){
+
+    if(currentPtr->vTimerPtr->enable){
+      // Timer has reached its desired value
+      if(currentPtr->vTimerPtr->current >= currentPtr->vTimerPtr->timeout){
+        currentPtr->vTimerPtr->flag = 1;
+        HAL_GPIO_TogglePin(GPIOA, (uint32_t) 1 << currentPtr->vTimerPtr->gpioPin);
+        // Check if timer is repetitive
+        if (currentPtr->vTimerPtr->repetitive){
+          currentPtr->vTimerPtr->current = 0;
+        }
+        else{
+          // Disable it if not repetitive
+          currentPtr->vTimerPtr->enable = 0;
+        }  
+      }
+      else{
+        // Normal increment
+        currentPtr->vTimerPtr->current++;
+        currentPtr->vTimerPtr->flag = 0;
+      }
+    }
+    // Get next node
+    currentPtr = currentPtr->nextNode;
+  }
+}
+
+/*--------------------------------------------------------------------------
+*	Name:			    TimerInit
+*	Description:	
+*	Parameters:		void
+*
+*	Returns:		  ret: CmdReturnOk = 0 if Okay.
+---------------------------------------------------------------------------*/
 void TIM3_IRQHandler(void)
 {
+  // This will call for "HAL_TIM_PeriodElapsedCallback()" on timer update 
   HAL_TIM_IRQHandler(&htim3);
 }
 
-// Callback whenever timer updates/overflows
-
+/*--------------------------------------------------------------------------
+*	Name:			    TimerInit
+*	Description:	
+*	Parameters:		void
+*
+*	Returns:		  ret: CmdReturnOk = 0 if Okay.
+---------------------------------------------------------------------------*/
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+  VirtualTimers();
 }
-
-// MACRO: Add new command to help menu
-ADD_CMD("timerinit", TimerInit,"\t\tInitializes timer.")
-
